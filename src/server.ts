1ca7f53b2830e07c5b7773b3aa0910866e28f0a3
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-13 12:22:14 -0400

    Phase 16

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
 import express from 'express'
 import router from './router'
+import db from './config/db'
+
+// Connecting to database
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('The connection to the database was success!')
+        
+    } catch (error) {
+        console.log(error)
+        console.log("There was an error in the database connection!")
+    }
+}
+
+connectDB()
 
 const server = express()
 
