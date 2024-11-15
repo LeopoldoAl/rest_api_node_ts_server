47dacd461dff3bac2b8f82c02866d04b6415819e
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-15 12:01:15 -0400

    Phase 35

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import  {body} from 'express-validator'
 import { createProduct } from "./handlers/product"
 
 const router = Router()
@@ -7,7 +8,19 @@ router.get('/', (req, res) => {
     res.json('From GET')
 })
 
-router.post('/', createProduct)
+router.post('/', 
+    // Validation
+    body('name').notEmpty()
+            .withMessage('The product name can not be empty!'),
+            
+    body('price')
+            .isNumeric().withMessage("Value is not valid!")
+            .notEmpty().withMessage('The product price can not be empty!')
+            .custom(value => value>0).withMessage('Value is not valid!'),
+    
+
+    
+    createProduct)
 
 router.put('/', (req, res) => {
     res.json('From PUT')
