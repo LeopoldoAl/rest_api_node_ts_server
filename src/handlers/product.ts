d33c89c423b081c2e6dcee55ff4d49beb81ace7c
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-18 08:25:02 -0400

    Phase 38

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from "express"
 import Product from "../models/Product.model"
-import { validationResult } from "express-validator"
 
 
 export const createProduct = async (req: Request, res: Response) => {
-
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        res.status(400).json({errors: errors.array()})
-        return 
+    try {
+        const product = await Product.create(req.body)
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+        
     }
-    const product = await Product.create(req.body)
-    res.json({data: product})
 }
