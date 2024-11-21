6ed562228797662a7eae35b14eb149c2d8a33ae6
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-21 08:59:18 -0400

    Phase 63

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -4,8 +4,11 @@ import server from "../server"
 describe('Get /api', () => {
     it('It should send back a json response', async () => {
         const res = await Request(server).get('/api')
-        expect(res.status).toBe(200)
-        expect(res.headers['content-type']).toMatch((/json/))
         expect(res.body.msg).toBe('From API')
+        expect(res.headers['content-type']).toMatch((/json/))
+        
+        expect(res.status).not.toBe(404)
+        expect(res.body.msg).not.toBe('from api')
+
     })
 })
